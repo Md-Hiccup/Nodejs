503c0bc98a037d101525f0069ffc1c1de6d14bcb
Md-Hiccup <ask2md@gmail.com>
2016-06-15 14:24:10 +0530

    json example

--- js/server.js
+++ js/server.js
@@ -6,21 +6,32 @@ var app = express();
 
 app.get('/', function (req , res){
     res.send("Hello World");
-})
+});
 
 app.get('/jsonObject' , function(req ,res){
     res.json({"name":"dastanHussain" , "age": "20"});
-})
+});
 
 app.get('/jsonArray' , function(req ,res){
     res.json(["dastanHussain","DastanIqbal "]);
-})
+});
+
+app.get('/jsonArray1' , function(req , res){
+    res.json([{"name": "dastanHussain", "age" : "20"},
+            {"name":"DastanIqbal", "age":"25"}])
+});
+
+app.get('/jsonArray2' , function(req ,res ){
+    res.json({"name": "hiccup", "age":"20" ,
+        "add":{ "block":"b3", "city":"newDelhi"}
+        })
+});
 
 var callback = function(){
-    var host = server.address().address
-    var port = server.address().port
+    var host = server.address().address ;
+    var port = server.address().port ;
 
     console.log("Example app listening at http://%s:%s", host, port)
-}
-var server = app.listen(8081,"192.168.32.18", callback)
+};
+var server = app.listen(8081,"192.168.32.18", callback) ;
 
